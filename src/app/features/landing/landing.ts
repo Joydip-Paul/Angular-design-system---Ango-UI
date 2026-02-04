7b3acbef24139953dbf7b81adab01c86796080e8
joydip-paul <joydippaul72@gmail.com>
2026-02-05 02:40:58 +0600

    landing update

--- src/app/features/landing/landing.ts
+++ src/app/features/landing/landing.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AtomicButton } from "../../lib/atomic/public-api";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AtomicButton],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
